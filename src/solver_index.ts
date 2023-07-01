39bfa7f741c3777c8c47cc47a1d84f21eef37c47
Nick Girardo <nickgirardo@gmail.com>
2023-07-01 13:52:27 -0400

    Cleanup: Remove manual perf testing code

--- src/solver_index.ts
+++ src/solver_index.ts
@@ -1,8 +1,5 @@
 import { render, h } from "preact";
 import { Solver } from "./components/Solver.js";
-import { solveBoard } from "./solver/index.js";
-import { Puzzle } from "./puzzle.js";
-import { Rand, range } from "./util/index.js";
 
 render(h(Solver, null, null), document.querySelector("#game-area")!);
 
@@ -23,56 +20,3 @@ const setupWorker = (): void => {
 };
 
 setupWorker();
-/*
-
-{
- const width = 16;
- const height = 16;
- const mineCount = 40;
- const startingTile = 0;
-
- const solvable: Array<number> = [];
- const unsolvable: Array<number> = [];
-
- const stepCounts = {
-   simple: 0,
-   pattern: 0,
-   subset: 0,
-   "mine-counter": 0,
- };
-
- for (const seed of range(1000)) {
-   if (seed % 50 === 0) console.log(seed);
-   const puzzle = new Puzzle(
-     width,
-     height,
-     mineCount,
-     startingTile,
-     new Rand(seed)
-   );
-
-   puzzle.checkTile(startingTile);
-   const solution = solveBoard(puzzle);
-
-   if (solution.solves) {
-     for (const step of solution.steps) {
-       stepCounts[step.solver]++;
-     }
-   }
-
-   if (solution.solves && !puzzle.checkSolution(solution))
-     console.log(
-       "something went wrong",
-       seed,
-       solution.puzzle.flagged,
-       puzzle.mines
-     );
-
-   if (solution.solves) solvable.push(seed);
-   else unsolvable.push(seed);
- }
-
- console.log(stepCounts);
-}
-
-*/
